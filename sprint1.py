afbe95ebf65c619281ed0577cec59a9efed8e3a4
CWeston98 <68019159+CWeston98@users.noreply.github.com>
2020-07-20 15:46:59 +0100

    tests passed

--- sprint1.py
+++ sprint1.py
@@ -7,17 +7,23 @@ class Rabbit:
         self.number_females = 1
         self.total = self.number_males + self.number_females
         self.months = 0
+        self.female_age = {3: 1}  # {age: count_with_age}
+        self.male_age = {3: 1}
 
-    def _set_initial(self, month=None, num_males=None, num_females=None):
+    def _set_initial(self, month=None, num_males=None, num_females=None, male_age=None, female_age=None):
         if not num_males:
             num_males = self.number_males
         if not num_females:
             num_females = self.number_females
         if not month:
             month = self.months
+        if not male_age:
+            male_age = self.male_age
+        if not female_age:
+            female_age = self.female_age
         total = num_males + num_females
         self.total = total
-        return [total, month]
+        return [total, month, male_age[3], female_age[3]]
 
     def advance_time(self, months=None):
         sleep(1)
@@ -26,7 +32,3 @@ class Rabbit:
         months += 1
         self.months = months
         return months
-
-xyz = Rabbit()
-print(xyz._set_initial())
-print(xyz.advance_time(3))
