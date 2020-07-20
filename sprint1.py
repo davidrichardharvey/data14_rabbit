b29599994c3e81ed0c323a8c48f41ea9c74e5252
David Harvey <dharvey@spartaglobal.com>
2020-07-20 16:31:25 +0100

    Merged from Juxhen branch

--- sprint1.py
+++ sprint1.py
@@ -1,5 +1,5 @@
 from time import sleep
-
+from random import randint
 
 class Rabbit:
     def __init__(self):
@@ -32,3 +32,10 @@ class Rabbit:
         months += 1
         self.months = months
         return months
+
+    def number_of_babies(self, pregnant_rabbits):
+        sum = 0
+        for i in range(pregnant_rabbits):
+            num_babies = randint(1, 14)
+            sum += num_babies
+        return sum
