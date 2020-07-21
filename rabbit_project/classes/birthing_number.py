c0f0c3fab3b1c1c1dbdd938e307cc59746d83f0a
CWeston98 <68019159+CWeston98@users.noreply.github.com>
2020-07-21 16:41:21 +0100

    Finally actually ready for testing

--- rabbit_project/classes/birthing_number.py
+++ rabbit_project/classes/birthing_number.py
@@ -1,7 +1,12 @@
-def birth_children():
-    new_children = randint(1,14)
-    return new_children
+from random import randint
 
 class Rabbits:
     def __init__(self):
+        self.pregnancies = [0,0]  # [ready_to_birth, new_pregnancies]
+        self.new_babies = 0
 
+    def birth_children(self):
+        self.new_babies = 0
+        for each in range(0, self.pregnancies[0])
+            new_children = randint(1,14)
+            self.new_babies += new_children
