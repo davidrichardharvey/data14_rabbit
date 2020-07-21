f0f57ef0c600c1c998d0f40fdec1e846a064292f
Alex <ASam@spartaglobal.com>
2020-07-21 16:50:45 +0100

    "Added method to find total population for rabbits"

--- rabbit_project/classes/rabbits.py
+++ rabbit_project/classes/rabbits.py
@@ -1,3 +1,18 @@
 class Rabbits:
     def __init__(self):
-        pass
+        self.total_population = 2
+        self.males = {3: 1, 2: 0, 1: 0}
+        self.females = {3: 1, 2: 0, 1: 0}
+
+    def total_population(self):
+        total_pop = 0
+        for i in self.males:
+            total_pop += (self.males[i])
+        for i in self.females:
+            total_pop += (self.females[i])
+        self.total_population = total_pop
+        return total_pop
+
+testing = Rabbits()
+
+print(testing.total_population)
