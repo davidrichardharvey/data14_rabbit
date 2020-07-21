92b385b15499e9ce2f3a20b5ff27abdc6c8ffa1d
Alex <ASam@spartaglobal.com>
2020-07-21 17:13:26 +0100

    "Appended a list for males and females for each month"

--- rabbit_project/classes/rabbits.py
+++ rabbit_project/classes/rabbits.py
@@ -1,18 +1,26 @@
 class Rabbits:
     def __init__(self):
-        self.total_population = 2
-        self.males = {3: 1, 2: 0, 1: 0}
-        self.females = {3: 1, 2: 0, 1: 0}
+        self.total_population = 0
+        self.males = 0
+        self.females = 0
+        self.list = self.init_list()
 
-    def total_population(self):
+    def calc_pop(self):
         total_pop = 0
-        for i in self.males:
-            total_pop += (self.males[i])
-        for i in self.females:
-            total_pop += (self.females[i])
+        for i in range(0, len(self.list)):
+            total_pop += (self.list[i]['Males'] + self.list[i]['Females'])
         self.total_population = total_pop
         return total_pop
 
-testing = Rabbits()
+    def init_list(self):
+        new_list = []
+        for each in range(0, 57):
+            new_list.append({"Males": 0, "Females": 0})
+        new_list.append({"Males": 1, "Females": 1})
+        new_list.append({"Males": 0, "Females": 0})
+        new_list.append({"Males": 0, "Females": 0})
+        return new_list
 
+testing = Rabbits()
+testing.calc_pop()
 print(testing.total_population)
