158e720a178def1cbbab148f568a725e73d058e6
David Harvey <dharvey@spartaglobal.com>
2020-07-21 14:07:33 +0100

    FRESH START!  Deleted all modules are tests

--- rabbit_project/Tests/model_time_test.py
+++
@@ -1,9 +0,0 @@
-from rabbit_project.rabbit import Rabbit
-
-test = Rabbit()
-
-
-def test_model_time():
-    assert test.advance_time(0) == 1
-    assert test.advance_time(60) == 61
-    assert test.advance_time(15) == 16
