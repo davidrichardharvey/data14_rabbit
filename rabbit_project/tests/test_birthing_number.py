3c50c974d564fdeedb40f3a6f3937ff655b138d0
eviedemetriou <edemetriou@spartaglobal.com>
2020-07-22 10:43:03 +0100

    test for birthing number working

--- rabbit_project/tests/test_birthing_number.py
+++ rabbit_project/tests/test_birthing_number.py
@@ -3,16 +3,16 @@ from rabbit_project.classes.birthing_number import Rabbits
 test = Rabbits()
 
 def test_birth_children():
-    assert test.pregnancies[0, 0] == 0  # Assert assumes that variable is already set as that number
+    assert test.pregnancies == [0, 0]  # Assert assumes that variable is already set as that number
     test.birth_children()
     assert test.new_babies == 0
-    test.pregnancies[0, 5] = 1
+    test.pregnancies = [0, 5]
     test.birth_children()
     assert test.new_babies == 0
-    test.pregnancies[7, 0] = 10
+    test.pregnancies = [7, 0]
     test.birth_children()
     assert 7 <= test.new_babies <= 98
-    test.pregnancies[10, 8] = 10
+    test.pregnancies = [10, 8]
     test.birth_children()
     assert 10 <= test.new_babies <= 140
 
