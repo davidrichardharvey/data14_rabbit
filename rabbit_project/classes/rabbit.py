17f1df0c1f88a91c3aa86cc4a11d39938d395d96
Alex <ASam@spartaglobal.com>
2020-07-22 15:25:56 +0100

    "Added a method to calculate pregnancies and, Added the run model based on time advancing,"

--- rabbit_project/classes/rabbit.py
+++ rabbit_project/classes/rabbit.py
@@ -1,5 +1,4 @@
 from random import randint
-import time as t
 
 
 class Rabbit:
@@ -8,18 +7,19 @@ class Rabbit:
         self.males = 1
         self.females = 1
         self.list = self.init_list()
-        self.pregnancies = [0, 0]
+        self.pregnancies = [0, 1]  # First index is the birthing females, second index is pregnant females
         self.new_babies = 0
-        self.month = 1
 
     def calculate_total_pop(self):
+        # This method calculated the total population per month and adds to the total population
         total_pop = 0
         for i in range(0, len(self.list)):
             total_pop += (self.list[i]['Males'] + self.list[i]['Females'])
-        self.total_population = total_pop
+        self.total_population += total_pop
         return total_pop
 
     def init_list(self):
+        # This sets the initial list of all ages of rabbits, assuming they die at 5 years old
         new_list = []
         for each in range(0, 57):
             new_list.append({"Males": 0, "Females": 0})
@@ -29,6 +29,7 @@ class Rabbit:
         return new_list
 
     def assign_genders(self):
+        # This assigns genders to rabbits at birth and appends these values to the list of rabbits
         males = 0
         females = 0
         for baby in range(0, self.new_babies):
@@ -40,20 +41,27 @@ class Rabbit:
         self.list.append({'Males': males, 'Females': females})
 
     def birth_children(self):
+        # This calculates the number of rabbits born based on the number of birthing females
         self.new_babies = 0
         for each in range(0, self.pregnancies.pop(0)):
             new_children = randint(1, 14)
             self.new_babies += new_children
 
     def calc_gender_totals(self):
+        # This calculates the total number of males and females in the rabbit list
         self.males = 0
         self.females = 0
         for generation in self.list:
             self.males += generation['Males']
             self.females += generation['Females']
 
-    def time_passing(self):
-        while True:
-            print(f"We are in month {self.month}")
-            t.sleep(1)
-            self.month += 1
+    def pregnant_babies(self):
+        # This calculates the number of rabbits that can be pregnant and returns a number of rabbits who become pregnant
+        fertile_males = self.males - self.list[-2]['Males'] - self.list[-1]['Males'] - self.pregnancies[1]
+        fertile_females = self.females - self.list[-2]['Females'] - self.list[-1]['Females'] - self.pregnancies[1]
+        pregnancies = min(fertile_females, fertile_males)
+        self.pregnancies.append(pregnancies)
+        return pregnancies
+
+
+
--- rabbit_project/time_functions.py
+++ rabbit_project/time_functions.py
@@ -1,9 +1,24 @@
 import time as t
+from rabbit_project.classes.rabbit import Rabbit
 
 
 def time_passing():
     month = 1
     while True:
         print(f"We are in month {month}")
+        testing.pregnant_babies()
+        testing.birth_children()
+        testing.assign_genders()
+        testing.calc_gender_totals()
+        testing.calculate_total_pop()
+        print(f"Population: {testing.total_population}")
+        print(f"Males: {testing.males}")
+        print(f"Females: {testing.females}")
         t.sleep(1)
         month += 1
+
+
+testing = Rabbit()
+
+
+time_passing()
