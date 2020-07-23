a9172b97e1e27324b2b77b711aabbd18067ea25e
BenSeifert <bseifert@spartaglobal.com>
2020-07-23 09:45:17 +0100

    model title included in csv write function

--- rabbit_project/data_storage.py
+++ rabbit_project/data_storage.py
@@ -4,10 +4,13 @@ import csv
 def create_rabbit_csv():
     headers_list = ["Months", " Total Population",
                     " Males", " Females", " Deaths"]
+    model_start = ["Model One:"]
     with open("rabbit_modelling_data", "w") as csvfile:
         csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(model_start)
         csv_writer.writerow(headers_list)
 
+# create_rabbit_csv()
 
 def append_data_csv():
     with open("rabbit_modelling_data", "a") as new_file:
