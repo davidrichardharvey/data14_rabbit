f16afc4d5aa89dde47d57db3e7b406732556424a
BenSeifert <bseifert@spartaglobal.com>
2020-07-23 10:19:43 +0100

    turned data_storage into class

--- rabbit_project/data_storage.py
+++ rabbit_project/data_storage.py
@@ -1,24 +1,41 @@
 import csv
 # from time_functions import the time function
 
-def create_rabbit_csv():
-    headers_list = ["Months", " Total Population",
+class RabbitCsv:
+
+    def __init__(self):
+        self.headers_list = ["Months", " Total Population",
                     " Males", " Females", " Deaths"]
-    model_start = ["Model One:"]
-    with open("rabbit_modelling_data", "w") as csvfile:
-        csv_writer = csv.writer(csvfile)
-        csv_writer.writerow(model_start)
-        csv_writer.writerow(headers_list)
+        self.model_count = 1
+        self.model_print = [f"Model {model_count}:"]
+        self.list_for_csv = []
 
-# create_rabbit_csv()
 
-def append_data_csv():
-    with open("rabbit_modelling_data", "a") as new_file:
-        csv_append = csv.writer(new_file)
-        # Refactor monthly outputs into list of lists --> talk to Alex & other devs
-        csv_append.writerows(list_for_csv)
+    def create_rabbit_csv(self):
+        with open("rabbit_modelling_data", "w") as csvfile:
+            csv_writer = csv.writer(csvfile)
+            csv_writer.writerow(model_print)
+            csv_writer.writerow(headers_list)
+
+
+    def append_data_csv(self):
+        with open("rabbit_modelling_data", "a") as rabbit_data:
+            csv_append = csv.writer(rabbit_data)
+            # Refactor monthly outputs into list of lists --> talk to Alex & other devs
+            csv_append.writerow(list_for_csv)
+
+    def advance_model(self):
+        with open("rabbit_modelling_data", "a") as model_move:
+            
+            # model_count += 1
+            # csv_append.writerow(model_print)
+            # csv_append.writerow(headers_list)
+
+    create_rabbit_csv()
+    # model_count += 1
+    # append_data_csv()
 
 # Code to be added to the time function:
-# list_for_csv = []
+
 # list_for_csv.append [month, total_population, males, females, deaths]
 
