fa7ed88e84def01e72335656c2739ae042934a0c
Jade <jadearthurs@icloud.com>
2020-07-23 16:45:14 +0100

    test for pregnant rabbits

--- rabbit_project/tests/test_pregnant_rabbits.py
+++ rabbit_project/tests/test_pregnant_rabbits.py
@@ -1,4 +1,5 @@
 from rabbit_project.classes.rabbit import Rabbit
+from scipy import stats
 
 test = Rabbit()
 
@@ -6,7 +7,8 @@ def test_pregnant_rabbits():
     assert test.males == 1
     assert test.females == 1
     assert test.pregnancies == [0, 1]
-    # assert test.pregnant_babies() == 1
+    assert stats.binom_test(test.pregnancies, test.females, 0.5, 'two-sided')
+
 
 
 
