9cac0167fea3d4246891785d347a603b0669014c
BenSeifert <bseifert@spartaglobal.com>
2020-07-24 10:47:59 +0100

    Graphing function complete

--- rabbit_project/time_functions.py
+++ rabbit_project/time_functions.py
@@ -2,6 +2,8 @@ import time as t
 import csv
 from rabbit_project.classes.rabbit import Rabbit
 import os
+import numpy as np
+import matplotlib.pyplot as plt
 
 rabbit_population_model = Rabbit()
 
@@ -29,6 +31,7 @@ def time_passing(final_month):
         month += 1
         year = int(month/12)
         rem_month = month % 12
+    draw_graph(model_count)
 
 
 def rabbit_one_month():
@@ -47,10 +50,24 @@ def append_data_csv(year, rem_month, total_population, males, females, deaths, m
 
 
 def create_rabbit_csv(model_count):
-    headers_list = ["Year", "Month", "Total Population",
+    headers_list = ["Year", "Month", "Population",
                     "Males", "Females", "Deaths"]
     with open(f"rabbit_modelling_data{model_count}.csv", "w", newline="") as csvfile:
         csv_writer = csv.writer(csvfile)
         csv_writer.writerow(headers_list)
 
 
+def draw_graph(model_count):
+    data = np.genfromtxt(f"rabbit_modelling_data{model_count}.csv", delimiter=",",
+                         names=["Year", "Month", "Population", "Males", "Females", "Deaths"])
+    ax = plt.plot
+    plt.suptitle(f"Rabbit Population Model {model_count}")
+    ax(data["Year"], data["Population"], color='r', label='Total Population')
+    ax(data["Year"], data["Males"], color='b', label='Males')
+    ax(data["Year"], data["Females"], color='g', label='Females')
+    ax(data["Year"], data["Deaths"], color='k', label='Deaths')
+    plt.xlabel('Time (Years)')
+    plt.ylabel('Number of Rabbits')
+    leg = plt.legend(('Total Population', 'Males', 'Females', 'Deaths'), loc='best')
+    leg.get_frame()
+    plt.show()
