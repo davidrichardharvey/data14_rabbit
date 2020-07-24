691d426c75be5c0c4e909a5e4237eeadc13e5edb
Your Name <you@example.com>
2020-07-24 10:58:58 +0100

    Comments added to rabbit death tests

--- rabbit_project/tests/test_rabbit_death.py
+++ rabbit_project/tests/test_rabbit_death.py
@@ -9,10 +9,10 @@ def test_rabbits_dead_new():
 
     test.list = []
     for i in range(0, 49):
-        test.list.append({"Males": 20, "Females": 20})
+        test.list.append({"Males": 20, "Females": 20}) # Resetting list so we can test input figures
 
     test.rabbit_deaths_new()
-    assert test.deaths_total in range(0, 41)
+    assert test.deaths_total in range(0, 41)  # Tests that the range of deaths is within viable boundaries
 
     test.list = []
     for i in range(0, 49):
@@ -23,10 +23,10 @@ def test_rabbits_dead_new():
 
     test.list = []
     for i in range(0, 49):
-        test.list.append({"Males": 1000, "Females": 0})
+        test.list.append({"Males": 1000, "Females": 0})  # Resests list and has values that allow us to test binomial
 
     test.rabbit_deaths_new()
-    assert stats.binom_test(test.deaths_total, test.vulnerable_males, 0.1, 'two-sided')
+    assert stats.binom_test(test.deaths_total, test.vulnerable_males, 0.1, 'two-sided')  # Tests that the binomial calcs work properly
 
     test.list = []
     for i in range(0, 49):
